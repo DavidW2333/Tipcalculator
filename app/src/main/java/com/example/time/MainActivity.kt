31375adca7f3b96953c6830e7ae07203708770d8
David Wang <david.wang023@gmail.com>
2022-12-13 08:56:45 +1300

    tip calc

---
+++ app/src/main/java/com/example/time/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.time
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.time.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedID = binding.tipOption.checkedRadioButtonId
+        val tipPercentage = when(selectedID){
+            R.id.option_eighteen_percentage -> 0.18
+            R.id.option_twenty_percent -> 0.20
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUP = binding.roundUpSwitch.isChecked
+        if (roundUP) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+
+
+
+    }
+}
